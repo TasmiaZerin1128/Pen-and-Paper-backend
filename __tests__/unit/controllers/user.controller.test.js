63d00328a13f7cc155aacc1b8fa5f3e4f6f0733a
TasmiaZerin1128 <bsse1128@iit.du.ac.bd>
2023-04-04 17:03:21 +0600

    issue 7 : test for blog controller complete

--- __tests__/unit/controllers/user.controller.test.js
+++ __tests__/unit/controllers/user.controller.test.js
@@ -1,7 +1,6 @@
 const userController = require('../../../controllers/user.controller');
 const userService = require('../../../services/user.service');
 const { sendResponse } = require('../../../utils/contentNegotiation');
-const { AppError } = require("../../../utils/errorHandler");
 const { userDB } = require('../mockDB');
 
 
@@ -79,7 +78,124 @@ describe('Testing userController module', () => {
             expect(response).toBe(expectedUser);
             expect(userService.getUserDTOByUsername).toHaveBeenCalledWith(req.params.username);
         });
+        it('should throw an error if userService call fails', async () => {
 
+            const req = {
+                params: {
+                    username: 'tasmia'
+                }
+            }
+
+            const res = {};
+            const next = jest.fn();
+
+            const expectedError = new Error('Internal Server Error');
+
+            jest
+                .spyOn(userService, 'getUserDTOByUsername')
+                .mockRejectedValueOnce(expectedError);
+
+            await userController.getUserByUsername(req, res, next);
+            expect(next).toHaveBeenCalledWith(expectedError);
+        });
+        
+    });
+
+    describe('Testing update User by Username', () => {
+        it('should update the user if exists and return 1', async () => {
+            const req = {
+                body: {
+                    password: '123456'
+                },
+                params: {
+                    username: 'tasmia'
+                }
+            }
+            const res = {};
+            const next = jest.fn();
+            const expectedResponse = { data : 'User updated' };
+
+            jest
+                .spyOn(userService, 'updateUser')
+                .mockReturnValueOnce(expectedResponse);
+
+            sendResponse.mockReturnValueOnce(expectedResponse);
+
+            response = await userController.updateUserByUsername(req, res, next);
+
+            expect(userService.updateUser).toBeCalledTimes(1);
+            expect(response).toBe(expectedResponse);
+            expect(userService.updateUser).toHaveBeenCalledWith(req.params.username, req.body);
+        });
+        it('should throw an error if userService call fails', async () => {
+
+            const req = {
+                body: {
+                    password: '123456'
+                },
+                params: {
+                    username: 'tasmia'
+                }
+            }
+
+            const res = {};
+            const next = jest.fn();
+
+            const expectedError = new Error('Internal Server Error');
+
+            jest
+                .spyOn(userService, 'updateUser')
+                .mockRejectedValueOnce(expectedError);
+
+            await userController.updateUserByUsername(req, res, next);
+            expect(next).toHaveBeenCalledWith(expectedError);
+        });
+        
+    });
+
+    describe('Testing delete User by Username', () => {
+        it('should delete the user if exists and return 1', async () => {
+            const req = {
+                params: {
+                    username: 'tasmia'
+                }
+            }
+            const res = {};
+            const next = jest.fn();
+            const expectedResponse = { data : 'User deleted' };
+
+            jest
+                .spyOn(userService, 'deleteUser')
+                .mockReturnValueOnce(expectedResponse);
+
+            sendResponse.mockReturnValueOnce(expectedResponse);
+
+            response = await userController.deleteUserByUsername(req, res, next);
+
+            expect(userService.deleteUser).toBeCalledTimes(1);
+            expect(response).toBe(expectedResponse);
+            expect(userService.deleteUser).toHaveBeenCalledWith(req.params.username);
+        });
+        it('should throw an error if userService call fails', async () => {
+
+            const req = {
+                params: {
+                    username: 'tasmia'
+                }
+            }
+
+            const res = {};
+            const next = jest.fn();
+
+            const expectedError = new Error('Internal Server Error');
+
+            jest
+                .spyOn(userService, 'deleteUser')
+                .mockRejectedValueOnce(expectedError);
+
+            await userController.deleteUserByUsername(req, res, next);
+            expect(next).toHaveBeenCalledWith(expectedError);
+        });
         
     })
 })
