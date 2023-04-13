3033c046340af816f08d119a7494f9bb73b9e24d
Tasmia Zerin <ttaosnmniya.6638.cross@gmail.com>
2023-04-13 22:36:36 +0600

    integration : fixed cors

--- app.js
+++ app.js
@@ -23,13 +23,6 @@ app.listen(PORT, () => {
 });
 
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // or a specific origin
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-})
-
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
