b4ebc6e25f80d6eb91571b450899a3ebc29ff32f
TasmiaZerin1128 <bsse1128@iit.du.ac.bd>
2023-04-17 16:58:49 +0600

    fixed cors

--- app.js
+++ app.js
@@ -22,7 +22,13 @@ app.listen(PORT, () => {
     console.log(`App started on ${PORT}`);
 });
 
-app.use(cors());
+app.use(cors(
+  { 
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+    credentials: true 
+  }));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
